ad54eda59649a8cd1a520b75d37b3f2f39b9c812
Foxyz24 <feliperamirez2004@gmail.com>
2023-09-27 20:40:06 -0400

    Reset my thinking

--- Diamond.java
+++ Diamond.java
@@ -5,23 +5,11 @@ public class Diamond {
 
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Enter a number: ");
-
     int number = 0;
 
-    int m;
-
-    int n;
-
-    try{
+    System.out.println("Enter a number: ");
 
     number = scan.nextInt();
-
-    }catch(Exception e){
-
-      System.out.println("Error: Please enter an integer value");
-
-    }
 
     if(number > 0){
 
@@ -33,29 +21,8 @@ public class Diamond {
 
         System.out.println("Its odd");
 
-
-        for (m = 1; m <= number; m++) {
-            for (n = 1; n <= number - m; n++) {
-                System.out.print(" ");
-            }
-            for (n = 1; n <= m * 2 - 1; n++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (m = number - 1; m > 0; m--) {
-            for (n = 1; n <= number - m; n++) {
-                System.out.print(" ");
-            }
-            for (n = 1; n <= m * 2 - 1; n++) {
-                System.out.print("*");
-            }
-            System.out.println();
-          }
-        }
-
-
       }
     }
 
   }
+}
