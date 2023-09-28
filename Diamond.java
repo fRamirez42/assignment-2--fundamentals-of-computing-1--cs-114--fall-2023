5b56184f8eb5c99927261d539af3c957de41eff6
Foxyz24 <feliperamirez2004@gmail.com>
2023-09-28 17:04:27 -0400

    Finished odd diamond

--- Diamond.java
+++ Diamond.java
@@ -5,21 +5,84 @@ public class Diamond {
 
     Scanner scan = new Scanner(System.in);
 
-    int number = 0;
+    int inputNumber = 0;
+
+    int stars = 1;
+
+    int spaces = 0;
 
     System.out.println("Enter a number: ");
 
-    number = scan.nextInt();
+    inputNumber = scan.nextInt();
 
-    if(number > 0){
+    if(inputNumber > 0){
 
-      if(number % 2 == 0){
+      if(inputNumber % 2 == 0){
 
         System.out.println("Its Even!");
 
       } else {
 
-        System.out.println("Its odd");
+        if(inputNumber != 1){
+
+          for(int i = 3; i <= inputNumber; i = i+2){
+
+            spaces++;
+
+          }
+
+        }
+
+        for(int i = 0; i <= inputNumber/2; i++)
+        {
+
+          for(int j = 1; j <= spaces; j++)
+          {
+
+            System.out.print(" ");
+
+          }
+
+          for(int j = 1; j<= stars; j++)
+          {
+
+            System.out.print("*");
+
+          }
+
+          System.out.println(" ");
+          stars = stars + 2;
+          spaces--;
+
+
+        }
+
+        stars = stars - 4;
+        spaces = spaces + 2;
+
+        for(int i = 0; i <= inputNumber/2; i++)
+        {
+
+          for(int j = 1; j <= spaces; j++)
+          {
+
+            System.out.print(" ");
+
+          }
+
+          for(int j = 1; j<= stars; j++)
+          {
+
+            System.out.print("*");
+
+          }
+
+          System.out.println(" ");
+          stars = stars - 2;
+          spaces++;
+
+
+        }
 
       }
     }
